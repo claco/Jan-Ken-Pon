01b394db18d3134dbe513d19f8439ddfc831ea50
Christopher H. Laco <claco@chrislaco.com>
2010-03-19 20:05:31 -0400

    Small tweak to StandardGameEngine now that I'm keeping it

--- app/models/standard_game_engine.rb
+++ app/models/standard_game_engine.rb
@@ -1,19 +1,24 @@
 class StandardGameEngine < GameEngine
-  @@weapons = [
-    Weapon.new(:id => 1, :name => 'Rock'),
-    Weapon.new(:id => 2, :name => 'Paper'),
-    Weapon.new(:id => 3, :name => 'Scissors')
-  ]
-
-  @@rules = [
-    lambda{ |a,b| b if a.weapon.id == 1 && b.weapon.id == 2 },
-    lambda{ |a,b| a if a.weapon.id == 1 && b.weapon.id == 3 },
-    lambda{ |a,b| a if a.weapon.id == 2 && b.weapon.id == 1 },
-    lambda{ |a,b| b if a.weapon.id == 2 && b.weapon.id == 3 },
-    lambda{ |a,b| b if a.weapon.id == 3 && b.weapon.id == 1 },
-    lambda{ |a,b| a if a.weapon.id == 3 && b.weapon.id == 2 }
-  ]
   def initialize
-    super( {:weapons => @@weapons, :rules => @@rules} )
+    super( {:weapons => weapons, :rules => rules} )
+  end
+
+  def rules
+    [
+      lambda{ |a,b| b if a.weapon.id == 1 && b.weapon.id == 2 },
+      lambda{ |a,b| a if a.weapon.id == 1 && b.weapon.id == 3 },
+      lambda{ |a,b| a if a.weapon.id == 2 && b.weapon.id == 1 },
+      lambda{ |a,b| b if a.weapon.id == 2 && b.weapon.id == 3 },
+      lambda{ |a,b| b if a.weapon.id == 3 && b.weapon.id == 1 },
+      lambda{ |a,b| a if a.weapon.id == 3 && b.weapon.id == 2 }
+    ]    
+  end
+
+  def weapons
+    [
+      Weapon.new(:id => 1, :name => 'Rock'),
+      Weapon.new(:id => 2, :name => 'Paper'),
+      Weapon.new(:id => 3, :name => 'Scissors')
+    ]
   end
 end
