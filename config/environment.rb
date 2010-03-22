c7f1aedd1642f9cb4d02c3e4f69540f63a6a011a
Christopher H. Laco <claco@chrislaco.com>
2010-03-22 01:24:54 -0400

    Modified gem config

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'mysql'
-  config.gem 'authlogic'
+  config.gem 'authlogic', :lib => false
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
